9ab2c4aabc4b1feffc3fcc8812f7fe59e19a95db
Dan Beaven <hackpingid@gmail.com>
2014-07-01 15:51:22 +0100

    no message

--- js/script.js
+++ js/script.js
@@ -25,3 +25,11 @@ $(document).ready(function(){
 	})
 })
 
+$(document).ready(function() {
+  $('.work').scrollToFixed();
+});
+
+$(document).ready(function() {
+  $('.nav').scrollToFixed();
+  $('.foot').scrollToFixed( { bottom: 0, limit: $('.foot').offset().top } );
+});
