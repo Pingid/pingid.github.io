e28c3eba000ca24ec5b4627b441c2ff3c4583d83
Dan Beaven <hackpingid@gmail.com>
2014-07-02 14:01:05 +0100

    Re built

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
+// On page load
+$(window).load(columnHeight);
+
+// On window resize
+$(window).resize( function () {
+    // Clear all forced column heights before recalculating them after window resize
+    $("#primary").css("height", "");  
+    $("#secondary").css("height", "");
+    $("#tertiary").css("height", "");
+    columnHeight();
+});
+
+// Make columns 100% in height
+function columnHeight() {
+    // Column heights should equal the document height minus the header height and footer height
+    var newHeight = $(document).height() - $(".site-header").height() - $(".site-footer").height() + "px";
+    $("#primary").css("height", newHeight);
+    $("#secondary").css("height", newHeight);
+    $("#tertiary").css("height", newHeight);
+}
+
 $(document).ready(function() {
 	$("#travel").click(function(){
 		
-		$(".work").addClass("pos")
-		$(".projects").addClass("pos")
-		$(".travel").removeClass("pos")
+		$(".work").addClass("position")
+		$(".projects").addClass("position")
+		$(".travel").removeClass("position")
 	})
 })
 
 $(document).ready(function(){
 	$("#work").click(function(){
 
-		$(".projects").addClass("pos")
-		$(".travel").addClass("pos")
-		$(".work").removeClass("pos")
+		$(".projects").addClass("position")
+		$(".travel").addClass("position")
+		$(".work").removeClass("position")
 	})
 })
 
@@ -24,8 +45,3 @@ $(document).ready(function(){
 		$(".projects").removeClass("pos")
 	})
 })
-
-// $(document).ready(function() {
-//   $('.contain').scrollToFixed();
-// });
-
