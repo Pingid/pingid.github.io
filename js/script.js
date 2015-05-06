017593b71805c7406b7dd0849520ed638c27b4f4
Dan <dm.beaven@gmail.com>
2015-05-06 19:07:15 +1000

    Re made to React side scroll

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var Page = React.createClass({
+  render: function(){
+    return (
+      <div className="page-wrapper">
+        <Scroller  />
+        <Line />
+      </div>
+    )
+  }
+});
+var Scroller = React.createClass({
+  render: function(){
+    return (
+      <div className="scroller">
+        <Picture />
+        <About />
+      </div>
+    )
+  }
+});
+var Line = React.createClass({
+  render: function(){
+    return (
+      <div className="line">
+        
+      </div>
+    )
+  }
+});
+var Picture = React.createClass({
+  render: function(){
+    return (
+      <div className="image-wrapper">
+        <img src="../img/me.png" />
+      </div>
+    )
+  }
+});
+var About = React.createClass({
+  render: function(){
+    return (
+      <div className="about">
+        <p>
+          London based Designer and developer Dan is best known for his intuitive clean Design.
+          Dan retains a firm belief in Design that fulfiles a function
+          never comprimising the User experience with indulging in unnecisery asthetics.
+        </p>
+      </div>
+    )
+  }
+})
+
+React.render(<Page />, document.getElementById('main'));
+// <p>
+//   London based Designer and developer Dan is best known for his intuitive clean Design.
+//   Dan retains a firm belief in Design that fulfiles a function
+//   never comprimising the User experience with indulging in unnecisery asthetics.
+// </p>
--- script.js
+++
@@ -1,6 +0,0 @@
-
-$.each( $('*'), function() { 
-    if( $(this).width() > $('body').width()) {
-        console.log("Wide Element: ", $(this), "Width: ", $(this).width()); 
-    } 
-});
