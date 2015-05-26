94a6da84d0c1a60672bc9b92baae98669c7462a2
Dan <dm.beaven@gmail.com>
2015-05-27 01:46:46 +1000

    compiled experiment to single doc

--- js/script.js
+++ js/script.js
@@ -78,10 +78,10 @@ var Experiments = React.createClass({
     return(
       <div className="experiments" style={this.props.active}>
         <div className="experiment">
-          <iframe src="../Canvas-Physics/index.html" frameborder="0" scrolling="no"></iframe>
+          <iframe src="experiment/Canvas-Physics/index.html" frameborder="0" scrolling="no"></iframe>
         </div>
         <div className="experiment">
-          <iframe src="../3D-JS/index.html" frameborder="0" scrolling="no"></iframe>
+          <iframe src="experiment/3D-JS.html" frameborder="0" scrolling="no"></iframe>
         </div>
       </div>
     )
