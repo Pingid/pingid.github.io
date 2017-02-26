101ee803340e7e6400ce7bed21c95f5c8ce1abe1
Daniel Beaven <dan@Daniels-MacBook-Air.local>
2017-02-26 19:08:35 +0000

    made responsive

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -4,11 +4,15 @@ import R from 'ramda';
 import classNames from 'classnames';
 import { Link } from 'react-router';
 
+import BlogThumbLarge from './BlogThumbLarge';
+import BlogThumbMedium from './BlogThumbMedium';
+
 import groupPostsBy3 from '../../utils/groupPostsBy3';
 import groupPostsBy2 from '../../utils/groupPostsBy2';
+import groupPostsBy1 from '../../utils/groupPostsBy1';
 
 const Header = () => (
-  <div className="pb3 pt2 pl3" style={{ borderBottom: '1px solid #a0a0a0' }}>
+  <div className="pb3 pt2 pl3" style={{}}>
     <h1 className="my3">Daniel Beaven</h1>
     <p style={{ maxWidth: '30rem' }}>
       This blog contains essays I have written as part of my degree course Interaction Design Arts. Each week I am required  to write up a 300 word post relating to the topic covered in that weeks lecture.
@@ -19,6 +23,7 @@ const Header = () => (
 const Blog = ({ blogPosts }) => {
   if (blogPosts.length < 1) return <div className="px3 mx-auto" style={{ maxWidth: '60rem' }}><Header /></div>;
   const group = (posts) => {
+    if (window.innerWidth < 590) return groupPostsBy1(posts)
     if (window.innerWidth < 878) return groupPostsBy2(posts)
     return groupPostsBy3(posts);
   }
@@ -27,50 +32,46 @@ const Blog = ({ blogPosts }) => {
     <div className="px3 mx-auto" style={{ maxWidth: '60rem' }}>
         <Header />
         <div className="flex flex-wrap border-box">
-          <div style={{ borderBottom: '1px solid #a0a0a0', flex: '0 0 100%' }}>
+          <div style={{ borderTop: '1px solid #a0a0a0', flex: '0 0 100%' }}>
             {
-              grouped.top ? (
-                  <Link to={'blog/' + grouped.top.title.split(' ').join('-')}>
-                    <div className="flex py2 px3 border-box items-start">
-                      <img role="presentation" className="pt3" style={{ flex: '0 0 66.66%', width: '66.66%' }} src={grouped.top.coverImage} />
-                      <div className="pl3 border-box self-start">
-                        <h1>{grouped.top.title}</h1>
-                        <p>{grouped.top.description}</p>
-                      </div>
-                    </div>
-                  </Link>
-              ) : null
+              window.innerWidth < 590 ? (
+                <div className="px3">
+                  <BlogThumbMedium
+                    title={grouped.top.title}
+                    description={grouped.top.description}
+                    image={{ src: grouped.top.coverImage }}
+                    borderLeft={false} />
+                </div>
+              ) : (
+                <BlogThumbLarge
+                  title={grouped.top.title}
+                  description={grouped.top.description}
+                  image={{ src: grouped.top.coverImage }} />
+              )
             }
           </div>
           {
             grouped.rest.map((section, i1) => (
-              <div className="flex py3 border-box" key={i1} style={{ borderBottom: '1px solid #a0a0a0' }}>
+              <div className="flex py3 border-box" key={i1} style={{ borderTop: '1px solid #a0a0a0' }}>
                 {
                   section.length < 1 ? null : section.map((post, i2) => {
-                    if (R.prop('coverImage', post)) return (
-                      <Link
-                        key={i2 + 10} to={'blog/' + post.title.split(' ').join('-')}
-                        className={classNames('px3 border-box', { 'c-border-l': i2 === 1 })}>
-                        <div className="flex border-box items-start">
-                          <img role="presentation" className="py1" style={{ flex: '0 0 50%', maxWidth: '50%' }} src={post.coverImage} />
-                          <div className="pl3 border-box">
-                            <h3 className="m0">{post.title}</h3>
-                            <p>{post.description}</p>
-                          </div>
-                        </div>
-                      </Link>
-                    );
+                    if (R.prop('coverImage', post))
+                      return <BlogThumbMedium
+                                key={i2 + 10}
+                                title={post.title}
+                                description={post.description}
+                                image={{ src: post.coverImage}}
+                                borderLeft={i2 === 1} />
                     return (
                       <div
                         key={i2 + 10}
                         className={classNames('px3 border-box', {
                           'c-border-x': i2 === 1 && section.length > 2,
                           'c-border-l': i2 === 1 && section.length === 2
-                        })}
-                        style={{ flex: '0 0 33.33%'}}>
+                        })}>
                         <Link to={'blog/' + post.title.split(' ').join('-')}>
                           <h3 className="m0 center">{post.title}</h3>
-                          <p className="center">{post.description}</p>
+                          <p className="mb0 center">{post.description}</p>
                         </Link>
                       </div>
                     );
@@ -79,6 +80,9 @@ const Blog = ({ blogPosts }) => {
               </div>
             ))
           }
+          <p className="flex items-center jusify-center fit p2" style={{ width: '100%' }}>
+            <a className="center" style={{ width: '100%' }} href="mailto:dm.beaven@gmail.com?Subject=hello">dm.beaven@gmail.com</a>
+          </p>
         </div>
     </div>
   )
---
+++ src/utils/groupPostsBy1.js
@@ -0,0 +1,7 @@
+import R from 'ramda';
+
+export default (posts) => {
+  const top = R.head(R.filter(x => (R.prop('coverImage', x) && R.length(R.prop('coverImage', x)) > 0 && !x.textWrap), posts));
+  const rest = posts.filter(x => x.title !== top.title).map(x => [x]);
+  return { top, rest };
+};
