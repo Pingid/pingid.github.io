b6548cd80effc308b68e49c474840639447ab36c
Dan <dan@Macs-MacBook-Pro.local>
2018-03-26 18:05:31 +0100

    switched to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import 'basscss/css/basscss.min.css';
 import './styles/index.css';
